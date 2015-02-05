5b8b41f052bc4bfef035df1eaefa0eb56a583d09
Shun Takebayashi <shun@takebayashi.asia>
2015-02-05 23:21:09 +0900

    Make scanning work with multithreads

--- lib/embulk/input_hbase.rb
+++ lib/embulk/input_hbase.rb
@@ -5,22 +5,40 @@ java_import org.apache.hadoop.hbase.client.HConnectionManager
 java_import org.apache.hadoop.hbase.client.Scan
 java_import org.apache.hadoop.hbase.util.Bytes
 java_import org.apache.hadoop.hbase.CellUtil
+java_import org.apache.hadoop.hbase.HConstants
 
 module Embulk
   class InputHBase < InputPlugin
     Plugin.register_input('hbase', self)
 
     def self.transaction(config, &control)
-      task = {
-        'host' => config.param('host', :string, default: 'localhost'),
-        'table' => config.param('table', :string)
-      }
-      threads = 1
+      host_name = config.param('host', :string, default: 'localhost')
+      table_name = config.param('table', :string)
+
       columns = config.param('columns', :array).map.with_index { |column, i|
         Column.new(i, column['name'], column['type'].to_sym)
       }
+
+      connection = connect(host_name)
+      table = connection.getTable(table_name)
+      regions = table.getRegionsInRange(HConstants::EMPTY_START_ROW, HConstants::EMPTY_END_ROW).map { |location|
+        location.getRegionInfo.getRegionNameAsString
+      }
+
+      task = {
+        'host' => host_name,
+        'table' => table_name,
+        'regions' => regions
+      }
+      threads = regions.size
       commit_reports = yield(task, columns, threads)
       return {}
+    end
+
+    def self.connect(host_name)
+      conf = HBaseConfiguration.create
+      conf.set('hbase.zookeeper.quorum', host_name)
+      HConnectionManager.createConnection(conf)
     end
 
     def initialize(task, schema, index, page_builder)
@@ -28,11 +46,10 @@ module Embulk
     end
 
     def run
-      conf = HBaseConfiguration.create
-      conf.set('hbase.zookeeper.quorum', @task['host'])
-      connection = HConnectionManager.createConnection(conf)
+      connection = self.class.connect(@task['host'])
       table = connection.getTable(@task['table'])
-      scan = Scan.new
+      region = connection.locateRegion(Bytes.toBytes(@task['regions'][@index])).getRegionInfo
+      scan = Scan.new(region.getStartKey, region.getEndKey)
       scanner = table.getScanner(scan)
       scanner.each { |result|
         @page_builder.add(@schema.map { |column|
